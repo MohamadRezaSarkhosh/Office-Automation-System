dc2c9abdf69bfb98afbc987e22f8e18fa7e2579a
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-05-17 00:29:47 +0330

    writing planner models

--- core/planner/models.py
+++ core/planner/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Operator(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.last_name}"
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+    hourly_efficiency = models.PositiveIntegerField(
+        help_text="Expected production per hour"
+    )
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class WorkSession(models.Model):
+    STATUS_CHOICES = [
+        ('good' , 'خوب'),
+        ('medium' , 'متوسط'),
+        ('bad' , 'بد')
+    ]
+    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    total_production = models.PositiveIntegerField()
+    performance_status = models.CharField(max_length=10, choices=STATUS_CHOICES, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.operator} - {self.device}"
+    
+    
